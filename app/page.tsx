6529bd5e3484caf13d29def5fb5ebfea9f2ea936
Gavin S. <stahlgavin@gmail.com>
2023-09-28 15:19:49 -0700

    landing page complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,21 @@
 "use client";
+import Link from "next/link";
 import { useUser } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import Link from "next/link";
 import { DarkmodeToggle } from "@/components/darkmode-toggle";
+import HeroBanner from "@/components/hero-banner";
 
 export default function Home() {
   const { isLoaded, isSignedIn, user } = useUser();
 
   return (
-    <div className="text-center">
-      <div className="flex justify-end p-6 gap-2">
+    <main>
+      <div className="absolute top-0 right-0 p-6 gap-2">
         <DarkmodeToggle />
         <UserButton afterSignOutUrl="/" />
       </div>
-      <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
-        <Button className="md:text-lg p-4 md:p-6">Get Started</Button>
-      </Link>
-    </div>
+      <HeroBanner />
+    </main>
   );
 }
---
+++ components/hero-banner.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { useUser } from "@clerk/nextjs";
+export default function HeroBanner() {
+  const { isLoaded, isSignedIn, user } = useUser();
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="underline decoration-rose-500 text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+        Nanotech Health
+      </h1>
+      <p className="text-white bg-slate-600 rounded-full mt-2 p-2 text-xs sm:text-sm md:text-base lg:text-lg">
+        Revolutionizing the way users interact with their healthcare
+      </p>
+      <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+        <Button className="md:text-lg p-4 m-4 md:p-6">Get Started</Button>
+      </Link>
+
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute z-[-1] w-auto sm:max-w-full lg:min-w-full min-h-full lg:max-w-none"
+      >
+        <source src="animated-background.mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+}
